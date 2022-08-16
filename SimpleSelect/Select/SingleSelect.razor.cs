7450a42968410f530ca24f6ba48eb297b394a75c
toqa-yaqout <toqayaqout31@outlook.com>
2022-08-16 11:59:28 +0200

    return selected student

--- SimpleSelect/Select/SingleSelect.razor.cs
+++ SimpleSelect/Select/SingleSelect.razor.cs
@@ -1,14 +1,17 @@
 ﻿namespace Components.Select;
-public partial class SingleSelect<TModel> where TModel : Entity
+public partial class SingleSelect<TEntity> where TEntity : Entity
 {
-    [Parameter] public List<TModel> Datasource { get; set; }
+    [Parameter] public List<TEntity> Datasource { get; set; }
     [Parameter] public string DefaultSelectLabel { get; set; }
+    [Parameter] public TEntity Value { get; set; }
+    [Parameter] public EventCallback<TEntity> ValueChanged { get; set; }
 
+    bool isOpen;
 
     protected override void OnInitialized()
     {
         if (Datasource is null)
-            Datasource = new List<TModel>();
+            Datasource = new List<TEntity>();
 
         if (string.IsNullOrEmpty(DefaultSelectLabel))
             DefaultSelectLabel = "Select";
@@ -16,9 +19,14 @@ public partial class SingleSelect<TModel> where TModel : Entity
         base.OnInitialized();
     }
 
-    bool isOpen;
     private void OnClickSelect()
     {
         isOpen = !isOpen;
+    }
+
+    private async void OnSelectItem(TEntity entity)
+    {
+        Value = entity;
+        await ValueChanged.InvokeAsync(entity);
     }
 }
